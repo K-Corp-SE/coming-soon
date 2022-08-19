6d0462f9167b994d4e302368d651ad926b122cde
Cclayelijah <Cclayelijah@gmail.com>
2022-08-19 03:42:26 +0000

    Deploying to gh-pages from @ K-Corp-SE/coming-soon@8fd2ac0497776631a6c22d35b0e71d09bd2eb753 🚀

---
+++ _next/static/pXqvnEfCjD1us5azpjkjZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-fe4cb81f2ec756e3.js","static/chunks/870-815fcfc173b2da27.js","static/chunks/pages/index-284407f3b0e10c63.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
